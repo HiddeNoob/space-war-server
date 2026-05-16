dfb70e2022f5b62e0969e3c378dee6b72dde8617
hiddenoob <alihangundogdu01@gmail.com>
2026-05-17 00:22:49 +0200

    proje yapısı

---
+++ src/main/java/com/hiddenoob/space_war_server/SpaceWarServerApplication.java
@@ -0,0 +1,13 @@
+package com.hiddenoob.space_war_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceWarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceWarServerApplication.class, args);
+	}
+
+}
