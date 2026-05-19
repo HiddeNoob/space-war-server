fc1728fbfb3f1947fc83f3c1bb338b5912c10deb
hiddenoob <alihangundogdu01@gmail.com>
2026-05-19 02:28:42 +0200

    game loop runs twice fixed.

--- src/main/java/com/hiddenoob/space_war_server/SpaceWarServerApplication.java
+++ src/main/java/com/hiddenoob/space_war_server/SpaceWarServerApplication.java
@@ -2,18 +2,14 @@ package com.hiddenoob.space_war_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import com.hiddenoob.space_war_server.server.Game;
 
 @SpringBootApplication
 @EnableAsync
 public class SpaceWarServerApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext  context = SpringApplication.run(SpaceWarServerApplication.class, args);
-		context.getBean(Game.class).start();
+		SpringApplication.run(SpaceWarServerApplication.class, args);
 	}
 
 }
