4491aa148c64f9e966211d4697fa95add1d5c56d
hiddenoob <alihangundogdu01@gmail.com>
2026-09-22 17:06:48 +0300

    changed socket relative to connection type

--- src/main/typescript/src/config.ts
+++ src/main/typescript/src/config.ts
@@ -1,5 +1,7 @@
 export const CONFIG = {
     SERVER_TICK_RATE: 32,
     SERVER_TICK_INTERVAL: 1000 / 32,
-    WS_URL: `ws://${window.location.host}/ws/game`
+    WS_URL: `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/ws/game`
 };
+
+    
